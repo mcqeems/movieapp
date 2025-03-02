f464642357ab56f7234354b073f730c2ebc6bc46
mcqeems <mcqeemsofficial@gmail.com>
2025-03-03 00:34:35 +0700

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+function Search({ searchTerm, setSearchTerm }) {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" />
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Search;
